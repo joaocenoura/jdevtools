e27159b775d57ae0830cafc3fd205378864033d9
Joao Rodrigues <jlrodrigues87@gmail.com>
2015-06-09 22:10:38 +0100

    issue #6 - removed shell scripts for backup and restore backup and restore process is done via ansible only (task.yml)

--- provisioning/roles/common/files/functions.sh
+++ provisioning/roles/common/files/functions.sh
@@ -36,3 +36,23 @@ function restore_tar {
 		echo "restore complete!"
 	fi
 }
+
+function get_newest_file {
+	# validate input
+	if [ $# -ne 2 ]
+	  then
+	    >&2 echo "expecting 2 arguments: <folder> <filename_match>"
+	    exit 1
+
+	fi
+
+	FILENAME=`ls $1 2> /dev/null | grep "$2" | sort | head -1` 
+
+	if [ -z "$FILENAME" ]; then
+		>&2 echo "couldn't find $2 in $1"
+		exit 2
+	else
+		echo $FILENAME
+		exit 0
+	fi
+}
